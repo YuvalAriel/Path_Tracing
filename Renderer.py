fixed screen rotation and added main.

Renderer.py
@@ -11,27 +11,31 @@ from Ray import Ray
 from Camera import Camera
 
 
+# (noam) read about fakes and mocks in tests
+# (noam) read about dependancy injection
 class Renderer:
-    def __init__(self, scene, camera):
+    def __init__(self, scene, camera, max_depth=2, num_samples=3, num_UV_samples=3):
         self.scene = scene
         self.camera = camera
-        self.max_depth = 2
-        self.num_samples = 3  # for antialiasing. number of rays per pixel.
-        self.num_UV_samples = 3
+        self.max_depth = max_depth
+        self.num_samples = num_samples  # for antialiasing. number of rays per pixel.
+        self.num_UV_samples = num_UV_samples
 
     def render(self):
         # w columns X h rows. each has a list of 3 zeroes. each pixel initialized to 0,0,0.
-        img = np.zeros((self.camera.img_width, self.camera.img_height, 3))
+        img = np.zeros((self.camera.img_height, self.camera.img_width, 3))
         for w in range(0, self.camera.img_width):
             for h in range(0, self.camera.img_height):
                 color = np.zeros(3)  # r,g,b = 0
                 for _ in range(0, self.num_samples):
-                    # create a ray from camera origin that goes through pixel w,h. ray
+                    # create a ray from camera origin that goes through pixel w,h.
                     ray = self.camera.gen_rand_ray(pixelx=w, pixely=h)
-                    color = color + self.compute_sample(ray, depth=0)  # needs to return color value as np.array rgb [0,1].
-                img[w, h, :] = np.clip(color / self.num_samples, 0, 1)
-        plt.imsave('UV.png', img)
+                    # needs to return color value as np.array rgb [0,1].
+                    color = color + self.compute_sample(ray, depth=0)
+                img[h, w, :] = np.clip(color / self.num_samples, 0, 1)
+        plt.imsave('pics\\UV.png', img)
 
+    # (noam) consider making these helper functions private
     def compute_sample(self, ray, depth):
         if depth > self.max_depth:
             return np.zeros(3)
@@ -57,28 +61,10 @@ class Renderer:
         return total_ray_emission
 
     def send_new_ray(self, obj_material, hit_position, norm, u, v, depth):
+        # (noam) Consider adding TODO with what you wish to implement here.
         # depending on material properties: will send new ray either as cone sample or as hemisphere sample.
         # at the moment, send new ray in direction of norm to the hit_position.
         reflected_ray = Ray(hit_position, norm)
 
         # recursive call
         return self.compute_sample(ray=reflected_ray, depth=depth + 1)
-
-
-red = [1, 0, 0]
-green = [0, 1, 0]
-blue = [0, 0, 1]
-redM = Material(red)
-greenM = Material(green)
-blueM = Material(blue)
-blueMm = Material([0.2,0.4,0.6])
-sphere1 = Sphere(center=(8, 0, -10), material=redM)
-sphere2 = Sphere(center=(-8, 0, -10), material=greenM)
-sphere3 = Sphere(center=(0, 0, -10))
-sphere4 = Sphere(center=(0, 5, -10), material=blueMm)
-
-scene = Scene(sphere1, sphere2, sphere3, sphere4)
-cam = Camera(120, 90)
-
-renderer = Renderer(scene, cam)
-renderer.render()